55643d681c5c6e3d0022a4dca9de70103e480180
박주현 <leehot2665@naver.com>
2022-10-25 00:55:22 +0900

    [Feat] controller, service 만듬 DB는 따로 사용하지 않음

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,9 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {}
+
+
     @Get()
     getAll() {
-        return "this will Return All Movies";
+        return this.moviesService.getAll();
+    }
+    @Get(':id')
+    getOne(@Param('id') movieId: number): Movie {
+      return this.moviesService.getOne(movieId);
+    }
+
+    @Post()
+    create(@Body() movieData : CreateMovieDto) {
+      
+      return this.moviesService.create(movieData);
+    }
+
+
+    @Delete(':id')
+    remove(@Param('id') movieId: number) {
+      return this.moviesService.deleteOne(movieId);
+    }
+
+    @Patch(':id')
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+        return this.moviesService.update(movieId, updateData);
     }
 }
